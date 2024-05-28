e23a1b813a20cc3ba88ed394bde20836566431fb
Brown <memoyebrown@gmail.com>
2024-05-28 13:41:03 +0100

    update config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,11 +1,6 @@
 const config = {
-  // development: {
   AIEndpoint: `${import.meta.env.VITE_BACKEND_URL}/api/v1/limewire`,
-  // backendUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/ai-text-to-image`,
-  // },
-  // production: {
-  //   backendUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/ai-text-to-image`,
-  // },
+  devAIEndpoint: `${import.meta.env.VITE_BACKEND_URL}/api/v1/limewire`,
 };
 
 export default config;
